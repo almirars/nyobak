b5ea78995c8862a5dc6d137361d7cb66b1f4fba4
almirars <64759903+almirars@users.noreply.github.com>
2021-01-04 09:17:46 +0700

    tambah product

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function () {  
     Route::get('/home', 'HomeController@index')->name('home');
     
+    Route::resource('/products','ProductController');
 });
 
 
